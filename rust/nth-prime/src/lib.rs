18c5466a43b2062bdb64f68535a6e54af95785e0
Longarithm <the.aleksandr.logunov@gmail.com>
2021-02-10 22:21:44 +0500

    Fix nth-prime solution

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,11 +1,8 @@
 pub fn nth(n: u32) -> u32 {
     let mut current_candidate: u32 = 2;
     let mut primes: Vec<u32> = Vec::new();
-    let mut len: u32 = 0;
     
-    // How to access primes.len() in the while loop?
-    // I got stuck because of getting "borrow of moved value" error
-    while len <= n {
+    while primes.len() <= n as usize {
         let mut divisible: bool = false;
         for i in primes.iter() {
             if current_candidate % i == 0 {
@@ -16,7 +13,6 @@ pub fn nth(n: u32) -> u32 {
 
         if !divisible {
             primes.push(current_candidate);
-            len += 1;
         }
 
         current_candidate += 1;
