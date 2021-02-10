e877e4ce2540602807680e8866c8ace6adf8cb5a
Longarithm <the.aleksandr.logunov@gmail.com>
2021-02-10 22:13:05 +0500

    Add gigasecond solution

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, Duration};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    return start + Duration::seconds(i64::pow(10, 9));
 }
